fae43d7040e79744cec2cd8766cd38a20ed52783
Misael Rivera <mrivera0709@gmail.com>
2017-08-31 20:44:20 -0400

    due date turn in

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function()
+	{
 
 	// Initial array of gifs
 	var topics = ["Ron Swanson", "Funny Star Wars", "Trump", "NBA"];
@@ -10,30 +11,30 @@ $(document).ready(function(){
 	function renderButtons() 
 		{
 	  // Remove currently existing buttons to avoid repeat buttons
-	  $("#buttons-view").empty();
-	  
-	  // Looping through array of gifs
-	  for (var j = 0; j < topics.length; j++) 
-	  	{
-	    
-	    // Create a button for each gif in the array
-	    var a = $("<button id=gifButtons>");
-	    
-	    // Adding a class of "gif" to the gif buttons
-	    a.addClass("gif");
-	    
-	    // Adding a data-attribute with the array as its value
-	    a.attr("data-name", topics[j]);
-	    
-	    // Make the existing "topics" as the text inside each gif button
-	    a.text(topics[j]);
-	    
-	    // Adding the button to the buttons-view div
-	    $("#buttons-view").append(a);
-	    
-	    //Empty the search bar after hitting search and creating the button.
-	    $("#gif-input").val("",);
-	  	}
+		$("#buttons-view").empty();
+		
+		// Looping through array of gifs
+		for (var j = 0; j < topics.length; j++) 
+			{
+			
+			// Create a button for each gif in the array
+			var a = $("<button id=gifButtons>");
+			
+			// Adding a class of "gif" to the gif buttons
+			a.addClass("gif");
+			
+			// Adding a data-attribute with the array as its value
+			a.attr("data-name", topics[j]);
+			
+			// Make the existing "topics" as the text inside each gif button
+			a.text(topics[j]);
+			
+			// Adding the button to the buttons-view div
+			$("#buttons-view").append(a);
+			
+			//Empty the search bar after hitting search and creating the button.
+			$("#gif-input").val("",);
+			}
 		}
 
 // =================================================================================
@@ -78,21 +79,21 @@ $(document).ready(function(){
 	// function to pull gifs from GIPHY api and display the html results
 	function displayGifInfo() 
 		{
-	  var gif = $(this).attr("data-name");
-	  var apiKey = "a4ccf65cb57740069f952abd8f4ec36c";
-	  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+	  	var gif = $(this).attr("data-name");
+	  	var apiKey = "a4ccf65cb57740069f952abd8f4ec36c";
+	  	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
 	        gif + "&api_key=" + apiKey + "&limit=10";
 
 // PULL DATA =======================================================================  
 	  // AJAX call triggered when the specific gif button is clicked
-	  $.ajax(
-	   	{
-	   	url: queryURL,
-	   	method: "GET"    
-	  	})
-	  	.done(function(response) 
-	  		{
-	  		console.log(response);
+	  	$.ajax(
+	   		{
+	   		url: queryURL,
+	   		method: "GET"    
+	  		})
+	  		.done(function(response) 
+	  			{
+	  			console.log(response);
 
 // LOOP THROUGH ARRAY ==============================================================
 								
@@ -102,48 +103,48 @@ $(document).ready(function(){
 				if (results.length === 0 )
 					{
 					$("#rightContent").html("<p id=noResults> No search results found </p>");	
-	      	}
+					}
 	      
-	      //Standard path to display gif data
-	      else 
-	      	{  
-		      //empty div to ensure previous htmldoes not remain
-		      $('#rightContent').empty();
-		      
-		      for (var i = 0; i < results.length; i++) 
-		      	{
-			      // Creating a div to hold the gif
-			      var gifDiv = $("<div>").addClass("gifDiv");
+				//Standard path to display gif data
+				else 
+					{  
+					//empty div to ensure previous htmldoes not remain
+					$('#rightContent').empty();
+					
+					for (var i = 0; i < results.length; i++) 
+						{
+						// Creating a div to hold the gif
+						var gifDiv = $("<div>").addClass("gifDiv");
 	     		
 // RATING ==========================================================================
-			      // Storing the rating data
-			      var rating = results[i].rating;
-			      
-			      // Creating an element to have the rating displayed
-			      var pOne = $("<p>").text("Rating: " + rating).attr("class", "rating");
-			      
-			      // Displaying the rating
-			      gifDiv.append(pOne);
+						// Storing the rating data
+						var rating = results[i].rating;
+						
+						// Creating an element to have the rating displayed
+						var pOne = $("<p>").text("Rating: " + rating).attr("class", "rating");
+						
+						// Displaying the rating
+						gifDiv.append(pOne);
 		      
 // GIF IMAGE =======================================================================      
 			      // Retrieving the URL for the image
 			      			      
-			      var imgURL = results[i].images.fixed_height_still.url;
-			      			      
-			      var gifURL = results[i].images.fixed_height.url;
-			      			      
-			      // Creating an element to hold the image
-			      var image = $('<img>').attr("src", imgURL).attr('data-animate', gifURL).attr('data-still', imgURL).attr('data-state', 'still').addClass("gifImage");
-		        	      
-			      // Appending the image
-			      gifDiv.append(image);
-			      		      
-			      // Putting the entire gif above the previous gifs
-			      $("#rightContent").prepend(gifDiv);
+						var imgURL = results[i].images.fixed_height_still.url;
+										
+						var gifURL = results[i].images.fixed_height.url;
+										
+						// Creating an element to hold the image
+						var image = $('<img>').attr("src", imgURL).attr('data-animate', gifURL).attr('data-still', imgURL).attr('data-state', 'still').addClass("gifImage");
+								
+						// Appending the image
+						gifDiv.append(image);
+									
+						// Putting the entire gif above the previous gifs
+						$("#rightContent").prepend(gifDiv);
 			    	 	    	
-		    		} //for loop
+		    			} //for loop
 					}
-	    	}); //done
+	    		}); //done
 	 	}
 
 // =================================================================================
@@ -153,39 +154,39 @@ $(document).ready(function(){
 function gifPlay()
 	{
 	var state = $(this).attr('data-state');
-		// if currently paused:
-		if ( state == 'still')
-			{
-			// change image to animated image(gif)
-      $(this).attr('src', $(this).data('animate'));
-      //change state to animate
-      $(this).attr('data-state', 'animate');
+	// if currently paused:
+	if ( state == 'still')
+		{
+		// change image to animated image(gif)
+    	$(this).attr('src', $(this).data('animate'));
+      	//change state to animate
+    	$(this).attr('data-state', 'animate');
 			
-			//change border color to green with a slow transition
+		//change border color to green with a slow transition
    		$(this)
    			.css({border: '3px solid rgba(0, 255, 131, 1)'})
    			.animate({borderWidth: 3}, 
    			50000);
      
-      console.log("PLAYING");
-      }
+      	console.log("PLAYING");
+      	}
     // if currently playing:
     else
     	{
       // change image to still image(gif)
-      $(this).attr('src', $(this).data('still'));
+      	$(this).attr('src', $(this).data('still'));
        //change state to still
-      $(this).attr('data-state', 'still');
+      	$(this).attr('data-state', 'still');
       
-      console.log("PAUSED")
+      	console.log("PAUSED")
 			
-			//change border color back to purple with a slow transition
+		//change border color back to purple with a slow transition
    		$(this)
    			.css({border: '3px solid #C8158D'})
    			.animate({borderWidth: 3}, 
    			50000);
 				
-      };
+      	};
 
 	};
 
@@ -197,7 +198,7 @@ $(document).on('click', '.gifImage', gifPlay);
 $(document).on('click', '#trending', function()
 	{
 	// AJAX call triggered when the trending button is clicked
-	var trendingURL = "https://api.giphy.com/v1/gifs/trending?api_key=a4ccf65cb57740069f952abd8f4ec36c" + "&limit=10";
+	var trendingURL = "https://api.giphy.com/v1/gifs/trending?api_key=a4ccf65cb57740069f952abd8f4ec36c&limit=10";
     $.ajax(
     	{
       url: trendingURL,
